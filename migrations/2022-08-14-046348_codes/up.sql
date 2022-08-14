a26797ca33c87760cc95cfefded404d136071c51
earlgray <earlgray283@gmail.com>
2022-08-14 14:55:46 +0900

    fix: language type

--- migrations/2022-08-14-046348_codes/up.sql
+++ migrations/2022-08-14-046348_codes/up.sql
@@ -2,7 +2,7 @@ CREATE TABLE codes (
     id SERIAL PRIMARY KEY,
     title VARCHAR NOT NULL,
     code_url VARCHAR NOT NULL,
-    language VARCHAR,
+    language Language NOT NULL,
     author_id INTEGER NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP,

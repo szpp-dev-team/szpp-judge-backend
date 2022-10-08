aefccda51e4cb899f5ff52bb0fa8f0ebf0a9677a
earlgray <earlgray283@gmail.com>
2022-10-08 23:52:52 +0900

    feat: fetch_by_session

--- src/db/repository/user.rs
+++ src/db/repository/user.rs
@@ -7,6 +7,7 @@ use diesel::{insert_into, prelude::*};
 
 pub trait UserRepository {
     fn fetch_user_by_id(&self, id: i32) -> Result<User>;
+    fn fetch_by_session(&self, session: &str) -> Result<User>;
     fn insert_user(&self, new_user: &NewUser) -> Result<User>;
 }
 
@@ -15,6 +16,15 @@ impl UserRepository for PgPool {
         use crate::schema::users::dsl::*;
         let mut conn = self.get()?;
         let res = users.filter(id.eq(target_id)).first(&mut conn)?;
+        Ok(res)
+    }
+
+    fn fetch_by_session(&self, session_str: &str) -> Result<User> {
+        use crate::schema::users::dsl::*;
+        let mut conn = self.get()?;
+        let res = users
+            .filter(session_token.eq(session_str))
+            .first(&mut conn)?;
         Ok(res)
     }
 

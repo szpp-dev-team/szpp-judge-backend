c0ec91784fdc2325513fea49d8591cd2fe2574f4
earlgray <earlgray283@gmail.com>
2022-10-09 14:55:51 +0900

    feat: logger

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 extern crate diesel;
 
 use crate::server::endpoint::{health_check::handle_check_health, users::handle_get_user};
-use actix_web::{web::Data, App, HttpServer};
+use actix_web::{middleware::Logger, web::Data, App, HttpServer};
 use anyhow::Result;
 use db::new_pg_pool;
 use dotenv::dotenv;
@@ -19,10 +19,12 @@ const PORT: u16 = 8080;
 async fn main() -> Result<()> {
     dotenv().ok();
 
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     let db_pool = Arc::new(new_pg_pool(env::var("DATABASE_URL")?.as_str())?);
 
     HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .app_data(Data::new(db_pool.clone()))
             .service(handle_register_user)
             .service(handle_get_user)

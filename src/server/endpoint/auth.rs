3e316113a4423cfe9ffddb28b54fdc0ef5e9d8a0
evipepota <meimuyuta@gmail.com>
2022-10-24 04:12:17 +0900

    signinini

--- src/server/endpoint/auth.rs
+++ src/server/endpoint/auth.rs
@@ -1,5 +1,10 @@
-use crate::{db::PgPool, server::model::auth::SigninPayload};
+use crate::{
+    db::{repository::auth::AuthRepository, PgPool},
+    server::model::auth::SigninPayload,
+    util::hash_password,
+};
 use actix_web::{
+    error::{ErrorInternalServerError, ErrorUnauthorized},
     post,
     web::{Data, Json},
     HttpResponse,
@@ -10,5 +15,11 @@ pub async fn handle_signin(
     db_pool: Data<PgPool>,
     data: Json<SigninPayload>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    unimplemented!()
+    let password = &data.password;
+    let encrypted_password = hash_password(password);
+    let mut conn = db_pool.get().map_err(ErrorInternalServerError)?;
+    match conn.fetch_user_by_credential(&data.username, &encrypted_password) {
+        Ok(s) => Ok(HttpResponse::Ok().json(&s)),
+        Err(e) => Err(ErrorUnauthorized(e)),
+    }
 }
